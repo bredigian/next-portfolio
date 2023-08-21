a8256e0ae16f7de9d904f1e666e205f737d42f6a
Gianluca Bredice - Nitro 5 <bredicegian@gmail.com>
2023-08-21 19:39:24 -0300

    [feature] projects page

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,9 +1,66 @@
-import React from "react"
+"use client"
+
+import { motion } from "framer-motion"
+import { projects } from "@/constants/projects"
 
 const Projects = () => {
   return (
-    <main className="grid place-items-center w-full">
-      <h1 className="">Projects</h1>
+    <main className="flex flex-col items-center gap-4 w-full mx-auto p-8">
+      <motion.h1
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.3, delay: 0.25 }}
+        className="text-6xl text-white"
+      >
+        Experience
+      </motion.h1>
+      <motion.p
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.3, delay: 0.75 }}
+        className="text-base text-gray-300"
+      >
+        Projects which helped me to reach the current level of experience
+      </motion.p>
+      <div className="flex flex-wrap items-center justify-center max-w-[1000px] gap-4">
+        {projects
+          .sort((a, b) => {
+            return b.date - a.date
+          })
+          .map((project, index) => {
+            const delay = 1.25 + index * 0.1
+            const dateFormatted = project.date
+              .toString()
+              .split(" ")
+              .slice(1, 4)
+              .join(" ")
+            return (
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.3, delay }}
+                key={project.name}
+                className="flex flex-row items-center justify-between bg-gray-100 w-full h-[120px] max-w-[400px] rounded-3xl overflow-hidden"
+              >
+                <span className="bg-yellow-primary h-full w-[40px]"></span>
+                <div className="flex flex-col items-start p-4 gap-2 text-gray-300 hover:text-white duration-200 ease-in-out max-w-[360px]">
+                  <h1 className="text-base">{project.name}</h1>
+                  <p className="text-xs">{project.description}</p>
+                  <div className="flex items-center justify-between w-full">
+                    <p className="text-xs">{dateFormatted}</p>
+                    <a
+                      href={project.url}
+                      target="_blank"
+                      className="text-xs hover:underline"
+                    >
+                      Deploy
+                    </a>
+                  </div>
+                </div>
+              </motion.div>
+            )
+          })}
+      </div>
     </main>
   )
 }
---
+++ src/constants/projects.js
@@ -0,0 +1,36 @@
+export const projects = [
+  {
+    name: "Casatomada",
+    description:
+      "Drinks store developed with JavaScript, SASS, Bootstrap and Firebase",
+    url: "https://casatomada.netlify.app",
+    date: new Date(2022, 6, 28),
+  },
+  {
+    name: "brg's Shop",
+    description:
+      "E-commerce developed with React, Redux, Firebase, Bootstrap and SASS",
+    url: "https://brgshop.netlify.app",
+    date: new Date(2022, 7, 23),
+  },
+  {
+    name: "Casatomada - App",
+    description: "Mobile app developed with React Native, Redux and Firebase",
+    url: "https://expo.dev/@bredigian/appCasatomada",
+    date: new Date(2022, 12, 22),
+  },
+  {
+    name: "Iluminarte",
+    description:
+      "Candle website developed with React, TailwindCSS, NodeJS and MySQL",
+    url: "https://dev-iluminarte.netlify.app",
+    date: new Date(2023, 6, 4),
+  },
+  {
+    name: "SG Pharma",
+    description:
+      "Pharmacy company website developed with React, TailwindCSS, NodeJS and MySQL",
+    url: "https://dev-sgpharma.netlify.app",
+    date: new Date(Date.now()),
+  },
+]
